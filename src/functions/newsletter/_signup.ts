547bde74e7a92de921efd312f15fb9528d706d90
André Duarte <mail@andreduarte.net>
2021-08-02 15:33:05 +0000

    Implement anonymousId, identify and new segment write key for dev

--- src/functions/newsletter/_signup.ts
+++ src/functions/newsletter/_signup.ts
@@ -1,15 +1,31 @@
-import Analytics from 'analytics-node';
+import Analytics from "analytics-node";
 import type { Response } from "@netlify/functions/src/function/response";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 
-const analytics = new Analytics('14WvAwP23g2AZddYGxblnp1BbVAErnsy');
+interface SignupInfo {
+  email: string;
+  anonymousId?: string;
+}
 
-export const signup = async (body: string): Promise<Response> => {
+const analytics =
+  process.env.NODE_ENV === "production"
+    ? new Analytics("14WvAwP23g2AZddYGxblnp1BbVAErnsy")
+    : new Analytics("CkSXNrLHHASi1g1aSTIUeaAq5a4W0o9B");
+
+export const signup = async (info: SignupInfo): Promise<Response> => {
+  if (info.anonymousId) {
+    analytics.identify({
+      anonymousId: info.anonymousId,
+      traits: {
+        email_untrusted: info.email,
+      },
+    });
+  }
+
   analytics.track({
-    anonymousId: nanoid(),
-    event: 'Signed Up',
-    email_untrusted: body,
-    signup_at: new Date().toISOString(),
+    anonymousId: info.anonymousId || nanoid(),
+    event: "newsletter_signup",
+    email: info.email,
   });
 
   return {
--- src/functions/newsletter/index.ts
+++ src/functions/newsletter/index.ts
@@ -1,8 +1,13 @@
 import type { Handler } from "@netlify/functions";
+import { cookie } from "cookie";
 import { signup } from "./_signup";
 
 const handler: Handler = (event, _, callback) => {
-  signup(event.body)
+  const anonymousId = cookie
+    .parse(event.headers.cookie)
+    .ajs_anonymous_id?.replace(/\"/g, "");
+
+  signup({ email: event.body, anonymousId })
     .then((response) => callback(null, response))
     .catch((error) => {
       console.error(error);
